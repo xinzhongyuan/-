232a19838795ee2e7900473271cc21013bcdcc9b
xinzhongyuan <xzy19901208@126.com>
2019-06-24 00:00:15 +0800

    提交

---
+++ src/main/java/com/leyou/mapper/TbCitiesMapper.java
@@ -0,0 +1,30 @@
+package com.leyou.mapper;
+
+import com.leyou.pojo.TbCities;
+import com.leyou.pojo.TbCitiesExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbCitiesMapper {
+    int countByExample(TbCitiesExample example);
+
+    int deleteByExample(TbCitiesExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(TbCities record);
+
+    int insertSelective(TbCities record);
+
+    List<TbCities> selectByExample(TbCitiesExample example);
+
+    TbCities selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") TbCities record, @Param("example") TbCitiesExample example);
+
+    int updateByExample(@Param("record") TbCities record, @Param("example") TbCitiesExample example);
+
+    int updateByPrimaryKeySelective(TbCities record);
+
+    int updateByPrimaryKey(TbCities record);
+}
---
+++ src/main/java/com/leyou/mapper/TbSpecificationMapper.java
@@ -0,0 +1,30 @@
+package com.leyou.mapper;
+
+import com.leyou.pojo.TbSpecification;
+import com.leyou.pojo.TbSpecificationExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbSpecificationMapper {
+    int countByExample(TbSpecificationExample example);
+
+    int deleteByExample(TbSpecificationExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbSpecification record);
+
+    int insertSelective(TbSpecification record);
+
+    List<TbSpecification> selectByExample(TbSpecificationExample example);
+
+    TbSpecification selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbSpecification record, @Param("example") TbSpecificationExample example);
+
+    int updateByExample(@Param("record") TbSpecification record, @Param("example") TbSpecificationExample example);
+
+    int updateByPrimaryKeySelective(TbSpecification record);
+
+    int updateByPrimaryKey(TbSpecification record);
+}
